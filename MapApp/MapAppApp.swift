cf4b391cf2d2180468e914134aaf333f6b9bb344
Vojtěch Kalivoda <kalivoda167@gmail.com>
2023-02-16 21:05:57 +0100

    Initial Commit

---
+++ MapApp/MapAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapAppApp.swift
+//  MapApp
+//
+//  Created by Vojtěch Kalivoda on 16.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct MapAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
